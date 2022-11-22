b5f5a083a122b362796beb5b0674da72a05ccfd9
Chen Qiu <qiu.chen1@northeastern.edu>
2022-11-21 23:10:30 -0800

    a9 Mongo fa22

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,37 +1,35 @@
-import posts from "./tuits.js";
-let tuits = posts;
-const createTuit = (req, res) => {
-    const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.dislikes = 0;
-    newTuit.liked = false;
-    tuits.push(newTuit);
-    res.json(newTuit);
-}
+import * as tuitsDao from "./tuits-dao.js";
 
-const findTuits = (req, res) => {
-    res.json(tuits)
-}
-const updateTuit = (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const tidUpdate = parseInt(tuitdIdToUpdate);
+const createTuit = async (req, res) => {
+  const newTuit = req.body;
+  newTuit.likes = 0;
+  newTuit.dislikes = 0;
+  newTuit.liked = false;
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
+};
 
-    const updates = req.body;
-    const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate)
-    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
-    res.sendStatus(200);
-}
-const deleteTuit = (req, res) => {
-    const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter((t) =>
-    t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
-}
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
+  res.json(tuits);
+};
+
+const updateTuit = async (req, res) => {
+  const tuitdIdToUpdate = req.params.tid;
+  const updates = req.body;
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+  res.json(status);
+};
+
+const deleteTuit = async (req, res) => {
+  const tuitdIdToDelete = req.params.tid;
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  res.sendStatus(200);
+};
 
 export default (app) => {
-    app.post('/api/tuits', createTuit);
-    app.get('/api/tuits', findTuits);
-    app.put('/api/tuits/:tid', updateTuit);
-    app.delete('/api/tuits/:tid', deleteTuit);
-}
+  app.post("/api/tuits", createTuit);
+  app.get("/api/tuits", findTuits);
+  app.put("/api/tuits/:tid", updateTuit);
+  app.delete("/api/tuits/:tid", deleteTuit);
+};
